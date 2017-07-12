d62523498cf37a2bc8b55f7b5c2c35253574b2d0
Paul Okeke <pugochukwu@vas-consulting.com>
2017-07-12 10:48:13 +0100

    allow all origin

--- route.js
+++ route.js
@@ -19,7 +19,7 @@ module.exports = function route() {
     app.set('port', process.env.PORT || 3000);
     // app.use(cors());
     app.use(function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', 'https://eu.apiconnect.ibmcloud.com');
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Credentials', "true");
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'apim-debug, x-travels-token, Content-Type, Accept, Origin');
