99a19b3132fe89a7f2c5b29748f111a88ae0e272
Paul Okeke <pugochukwu@vas-consulting.com>
2018-03-08 17:45:33 +0100

    test

--- modules/PaymentPlans/model/services/PaymentPlanService.js
+++ modules/PaymentPlans/model/services/PaymentPlanService.js
@@ -55,11 +55,6 @@ class PaymentPlanService extends ApiService {
         //enforce the validation
         let isValid = validate(paymentPlan.rules(), paymentPlan);
 
-        paymentPlan.amount = parseInt(paymentPlan.amount);
-        paymentPlan.waive_percentage = parseInt(paymentPlan.waive_percentage);
-        paymentPlan.disc_order_id = parseInt(paymentPlan.disc_order_id);
-        paymentPlan.balance = parseFloat(paymentPlan.balance);
-
         ApiService.insertPermissionRights(paymentPlan, who);
 
         if (!isValid) {
