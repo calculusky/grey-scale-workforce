25e3ab3bc2b75f7a652f7f442fc25cba05197d11
Paul Okeke <pugochukwu@vas-consulting.com>
2018-03-21 12:00:01 +0100

    Modified how group id is retrieve so as to conform to the new user_groups table implementation.

--- modules/ApiService.js
+++ modules/ApiService.js
@@ -18,7 +18,7 @@ class ApiService {
     static insertPermissionRights(data, who) {
         if (!who) return;
         data['created_by'] = who.sub;
-        if (!data['group_id']) data['group_id'] = who.group;
+        if (!data['group_id']) data['group_id'] = (Array.isArray(who.group)) ? who.group.shift() : 1;
         if (!data['assigned_to'] || data['assigned'].length === 0)
             data['assigned_to'] = JSON.stringify(
                 [{id: who.sub, 'created_at': Utils.date.dateToMysql(new Date(), 'YYYY-MM-DD H:m:s')}]
