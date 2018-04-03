09fa39876737e28a5bf2dd75ff403dd777093b48
Paul Okeke <pugochukwu@vas-consulting.com>
2018-04-03 19:33:07 +0100

    Add payment plan to work orders payload

--- modules/WorkOrders/model/services/WorkOrderService.js
+++ modules/WorkOrders/model/services/WorkOrderService.js
@@ -258,7 +258,7 @@ function sweepWorkOrderResponsePayload(workOrder) {
  *
  * @param workOrders
  * @param context
- * @param moduleName
+ * @param module
  * @param resolve
  * @param reject
  * @param isSingle
@@ -266,11 +266,12 @@ function sweepWorkOrderResponsePayload(workOrder) {
  * @param workTypes
  * @private
  */
-function _doWorkOrderList(workOrders, context, moduleName, resolve, reject, isSingle = false, groups, workTypes) {
+async function _doWorkOrderList(workOrders, context, module, resolve, reject, isSingle = false, groups, workTypes) {
     let rowLen = workOrders.length;
     let processed = 0;
-
-    workOrders.forEach(workOrder => {
+    const db = context.database;
+    // workOrders.forEach(workOrder => {
+    for (let workOrder of workOrders) {
         let promises = [];
 
         let workType = workOrder['type_name'] = workTypes[workOrder.type_id].name;
@@ -281,76 +282,51 @@ function _doWorkOrderList(workOrders, context, moduleName, resolve, reject, isSi
 
         //If we're loading for a list view let's get the counts of notes, attachments etc.
         if (!isSingle) {
-            let countNotes = context.database.count('note as notes_count').from("notes")
-                .where("module", moduleName).where("relation_id", workOrder.id);
+            let countNotes = db.count('note as notes_count').from("notes")
+                .where("module", module).where("relation_id", workOrder.id);
 
-            let countAttachments = context.database.count('id as attachments_count').from("attachments")
-                .where("module", moduleName).where("relation_id", workOrder.id);
+            let countAttachments = db.count('id as attachments_count').from("attachments")
+                .where("module", module).where("relation_id", workOrder.id);
 
             promises.push(countNotes, countAttachments);
         }
-        //Promises in order of possible arrangement
-        //0: The related Entity e.g faults, disconnection_billing
-        //1: Notes Count
-        //2: Attachments Count
-        Promise.all(promises).then(values => {
-            //its compulsory that we check that a record exist
-            const relatedToRecord = values[0];
-            const notesCount = values[1];
-            const attachmentCount = values[2];
 
-            if (notesCount && attachmentCount) {
-                workOrder['notes_count'] = notesCount.shift()['notes_count'];
-                workOrder['attachments_count'] = attachmentCount.shift()['attachments_count'];
-            }
-
-            let wait = false;
-
-            //First thing lets get the work order type details
-            if (relatedToRecord && relatedToRecord.records.length) {
-                let relatedModel = relatedToRecord.records.shift();
-                if (relatedModel) {
-                    delete relatedModel['id'];
-                    delete relatedModel['created_at'];
-                    delete relatedModel['updated_at'];
-                }
-
-                workOrder[workType.toLowerCase()] = relatedModel;
-
-                switch (workOrder.related_to.toLowerCase()) {
-                    case "disconnection_billings":
-                        wait = true;
-                        //We know that a disconnection billing is related
-                        //To a customer so we need to get the customer details.
-                        relatedModel.customer().then(customer => {
-                            wait = false;
-                            workOrder['customer'] = customer.records.shift();
-                            if (!wait) {
-                                sweepWorkOrderResponsePayload(workOrder);
-                                if (++processed === rowLen) {
-                                    return resolve(Utils.buildResponse({data: {items: workOrders}}));
-                                }
-                            }
-                        });
-                        break;
-                    case "faults":
-
-                        break;
-                }
-            }
-            if (!wait) {
-                sweepWorkOrderResponsePayload(workOrder);
-                if (++processed === rowLen) {
-                    return resolve(Utils.buildResponse({data: {items: workOrders}}));
-                }
-            }
-        }).catch(err => {
+        const [relatedToRecord, notesCount, attachmentCount] = await Promise.all(promises).catch(err => {
             console.log(err);
             return reject(err);
         });
+        //its compulsory that we check that a record exist
+        if (notesCount && attachmentCount) {
+            workOrder['notes_count'] = notesCount.shift()['notes_count'];
+            workOrder['attachments_count'] = attachmentCount.shift()['attachments_count'];
+        }
+
+        //First thing lets get the work order type details
+        if (relatedToRecord && relatedToRecord.records.length) {
+            let relatedModel = relatedToRecord.records.shift();
+            if (relatedModel) {
+                delete relatedModel['created_at'];
+                delete relatedModel['updated_at'];
+            }
+            workOrder[workType.toLowerCase()] = relatedModel;
+            switch (workOrder.related_to.toLowerCase()) {
+                case "disconnection_billings":
+                    const [customer, plan] = await Promise.all([relatedModel.customer(), relatedModel.paymentPlan()]);
+                    workOrder['customer'] = customer.records.shift() || {};
+                    workOrder['payment_plan'] = plan.records.shift() || {};
+                    break;
+                case "faults":
+
+                    break;
+            }
+        }
+        sweepWorkOrderResponsePayload(workOrder);
+        if (++processed === rowLen) {
+            return resolve(Utils.buildResponse({data: {items: workOrders}}));
+        }
         //remove the request_id its irrelevant
         workOrder['work_order_no'] = Utils.humanizeUniqueSystemNumber(workOrder['work_order_no']);
-    });
+    }
 }
 
 module.exports = WorkOrderService;
