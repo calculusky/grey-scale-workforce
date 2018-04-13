204e2544986ef03bb325e5cd05e2e2873b00efde
Paul Okeke <pugochukwu@vas-consulting.com>
2018-04-13 01:23:40 +0100

    Bulk update in notifications. | updated the updated_at date during updates.

--- modules/Reports/controller/ReportController.js
+++ modules/Reports/controller/ReportController.js
@@ -6,8 +6,8 @@ const Log = require(`${__dirname}/../../../core/logger`);
 const RecognitionService = require('../../Users/model/services/RecognitionService');
 
 module.exports.controller = function (app, {API, jsonParser, urlencodedParser}) {
-    app.use('/reports*', (req, res, next)=>API.recognitions().auth(req, res, next));
-    
+    // app.use('/reports*', (req, res, next)=>API.recognitions().auth(req, res, next));
+
     /**
      * @swagger
      * /reports/{id}:
@@ -37,5 +37,63 @@ module.exports.controller = function (app, {API, jsonParser, urlencodedParser}) 
             });
     });
 
-    
+    /**
+     * @swagger
+     * /reports/{module}/{report_time}:
+     *   get:
+     *     summary: Get work order report
+     *     description: 'Retrieve numbers of work order record within the past time'
+     *     tags: [Reports]
+     *     produces:
+     *     - application/json
+     *     operationId: getModuleRecordCount
+     *     responses:
+     *       '200':
+     *         description: Successful
+     *         schema:
+     *           $ref: '#/definitions/getModuleRecordCountOutput'
+     *     parameters:
+     *     - $ref: '#/parameters/sessionId'
+     *     - $ref: '#/parameters/report_time'
+     */
+    app.get('/reports/:module/:time', urlencodedParser, (req, res) => {
+        return API.reports().getModuleRecordCountFromTime(req.params['module'], req.params['time'])
+            .then(({data, code}) => {
+                return res.status(code).send(data);
+            })
+            .catch(({err, code}) => {
+                return res.status(code).send(err);
+            });
+    });
+
+
+    /**
+     * @swagger
+     * /reports/payments/total_amount:
+     *   get:
+     *     summary: Get work order report
+     *     description: 'Retrieve numbers of work order record within the past time'
+     *     tags: [Reports]
+     *     produces:
+     *     - application/json
+     *     operationId: getModuleRecordCount
+     *     responses:
+     *       '200':
+     *         description: Successful
+     *         schema:
+     *           $ref: '#/definitions/getModuleRecordCountOutput'
+     *     parameters:
+     *     - $ref: '#/parameters/sessionId'
+     *     - $ref: '#/parameters/report_time'
+     */
+    app.get('/reports/payments/total_amount', urlencodedParser, (req, res) => {
+        return API.reports().getTotalAmountReceivedReport()
+            .then(({data, code}) => {
+                return res.status(code).send(data);
+            })
+            .catch(({err, code}) => {
+                return res.status(code).send(err);
+            });
+    });
+
 };
