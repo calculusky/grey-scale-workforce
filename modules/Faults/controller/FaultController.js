fb2360a964bb57d71cb54969dca066de5b420c0c
Paul Okeke <pugochukwu@vas-consulting.com>
2018-07-23 11:01:04 +0100

    Retrieve fault work orders

--- modules/Faults/controller/FaultController.js
+++ modules/Faults/controller/FaultController.js
@@ -101,6 +101,7 @@ module.exports.controller = function (app, {API, jsonParser, urlencodedParser, m
      *     - $ref: '#/parameters/sessionId'
      */
     app.get('/faults', urlencodedParser, (req, res) => {
+        console.log(req.query);
         return API.faults().getFaults(req.query, req.who).then(({data, code}) => {
             return res.status(code).send(data);
         }).catch(({err, code}) => {
@@ -135,6 +136,66 @@ module.exports.controller = function (app, {API, jsonParser, urlencodedParser, m
             return res.status((code) ? code : 500).send((err) ? err : "Internal Server Error")
         });
     });
+
+    /**
+     * @swagger
+     * /faults/{id}/work_orders:
+     *   get:
+     *     summary: Get work orders belonging to a fault
+     *     description: ''
+     *     tags: [Faults]
+     *     produces:
+     *     - application/json
+     *     operationId: getWorkOrders
+     *     responses:
+     *       '200':
+     *         description: Successful
+     *         schema:
+     *           $ref: '#/definitions/getWorkOrderOutput'
+     *     parameters:
+     *     - $ref: '#/parameters/sessionId'
+     *     - $ref: '#/parameters/fault_id'
+     */
+    app.get('/faults/:id/work_orders', urlencodedParser, (req, res) => {
+        const query = {relation_id: req.params['id'], type_id: 3, include: ""};
+        return API.workOrders().getWorkOrders(query, req.who).then(({data, code}) => {
+            return res.status(code).send(data);
+        }).catch(({err, code}) => {
+            return res.status((code) ? code : 500).send((err) ? err : "Internal Server Error")
+        });
+    });
+
+    /**
+     * @swagger
+     * /faults/{id}/notes/{offset}/{limit}:
+     *   get:
+     *     summary: "List Fault Notes"
+     *     description: ''
+     *     tags: [faults]
+     *     produces:
+     *     - application/json
+     *     operationId: getNotes
+     *     responses:
+     *       '200':
+     *         description: Successful
+     *         schema:
+     *           $ref: '#/definitions/getNoteOutput'
+     *     parameters:
+     *     - $ref: '#/parameters/sessionId'
+     *     - $ref: '#/parameters/fault_id'
+     *     - $ref: '#/parameters/offset'
+     *     - $ref: '#/parameters/limit'
+     */
+    app.get('/faults/:id/notes', urlencodedParser, (req, res) => {
+        return API.notes().getNotes(req.params['id'], "faults", "relation_id", req.who, req.query.offset || 0, req.query.limit || 10)
+            .then(({data, code}) => {
+                return res.status(code).send(data);
+            })
+            .catch(({err, code}) => {
+                return res.status(code).send(err);
+            });
+    });
+
 
     /**
      * @swagger
