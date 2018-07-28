a6959e734a99e388e73d08cf00232b6cfa615e35
Paul Okeke <pugochukwu@vas-consulting.com>
2018-07-28 22:14:11 +0100

    Fix issue with assigned_to value not setting default value

--- modules/ApiService.js
+++ modules/ApiService.js
@@ -20,13 +20,15 @@ class ApiService {
         data['created_by'] = who.sub;
         if (!data['group_id']) data['group_id'] = (Array.isArray(who.group)) ? who.group.shift() : 1;
 
-        if (!data['assigned_to'] || data['assigned_to'].length === 0) {
+        let assignedTo = data['assigned_to'];
+        const setDefaultAssignedTo = () => {
             data['assigned_to'] = JSON.stringify([
                 {id: who.sub, 'created_at': Utils.date.dateToMysql(new Date(), 'YYYY-MM-DD H:m:s')}
             ]);
-        }
+        };
+        if (typeof assignedTo === "string" && assignedTo === "[]" || assignedTo.length === 0) setDefaultAssignedTo();
+        else if (!assignedTo || assignedTo.length === 0) setDefaultAssignedTo();
     }
-
 }
 
 module.exports = ApiService;
