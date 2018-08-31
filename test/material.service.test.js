7e0aaabd7b1f12284b66099ee2dc0eb1ccf0953f
Paul Okeke <pugochukwu@vas-consulting.com>
2018-08-31 17:43:33 +0000

    Resolve "We should have routes for basic base records"

--- test/material.service.test.js
+++ test/material.service.test.js
@@ -2,9 +2,22 @@
  * Created by paulex on 6/02/18.
  */
 
+/**
+ * @type {API}
+ */
 const API = require('../index').test();
 
 
 it("Should fail when you try to create a material with empty data", () => {
     return expect(API.materials().createMaterial({}, {})).rejects.toEqual({});
+});
+
+
+test("Query for a list of materials", () => {
+    return expect(API.materials().getMaterials({
+        unit_price: "Metres"
+    }, {})).resolves.toEqual(expect.objectContaining({
+        code: expect.any(Number),
+        data: expect.any(Object)
+    }));
 });
