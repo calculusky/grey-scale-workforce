a796af0a494574c7548ab724bda98497159f6bed
Paul Okeke <pugochukwu@vas-consulting.com>
2018-11-19 20:34:35 +0100

    closes #47

--- core/Context.js
+++ core/Context.js
@@ -8,6 +8,7 @@ const MapperFactory = require('./factory/MapperFactory');
 const Utils = require("../core/Utility/Utils");
 const redis = require("redis");
 let globalContext = null;
+const moment = require('moment');
 
 //Private Fields
 let _privateStore = new WeakMap();
@@ -34,13 +35,16 @@ class Context {
                 "host": process.env.DB_HOST,
                 "user": process.env.DB_USER,
                 "password": process.env.DB_PASS,
-                "database": process.env.DB_DATABASE
-            },
-            pool: {
-                afterCreate: function(connection, callback) {
-                    connection.query("SET time_zone = '+02:00';", function(err) {
-                        callback(err, connection);
-                    });
+                "database": process.env.DB_DATABASE,
+                timezone: "UTC",
+                typeCast: function (field, next) {
+                    if (field.type === 'DATETIME' || field.type === 'TIMESTAMP') {
+                        const value = field.string();
+                        return (value === null)
+                            ? value
+                            : moment(value).utc().utcOffset(parseInt(process.env.TIME_ZONE)).format('YYYY-MM-DDTHH:mm:ssZ');
+                    }
+                    return next();
                 }
             }
         });
