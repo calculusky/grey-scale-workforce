9767263bf8ca4290b950afc504db3ed092cc9333
Paul Okeke <pugochukwu@vas-consulting.com>
2019-07-22 15:15:02 +0100

    fixes

--- processes/LegendService.js
+++ processes/LegendService.js
@@ -94,7 +94,8 @@ module.exports = (function () {
          */
         async requestMaterials(faultId, materials = [], group = {}) {
             _checkInitialized();
-            return await materials.filter(i => i['category']['source'] && i['category']['source'] === 'ie_legend').reduce(async (acc, curr) => {
+            console.log(faultId);
+            return await materials.filter(i => i['source'] && i['source'] === 'ie_legend').reduce(async (acc, curr) => {
                 const _accumulator = await acc;
                 const mResponse = await this.requestMaterial(faultId, curr, group);
                 _accumulator.push(mResponse);
@@ -110,13 +111,15 @@ module.exports = (function () {
          * @return {Promise<any>}
          */
         requestMaterial(faultId, material = {}, group = {}) {
+            console.log("SECIND", faultId);
             _checkInitialized();
             if (!faultId) throw new Error("The Fault ID is required");
             const executor = (resolve, reject) => {
                 //TODO use validate
                 if (!itemTypes[material.category.id]) return reject("ItemTypes not found");
+                console.log('PASSING',faultId);
                 const _options = {...options};
-                const legendMatRequest = {Fault_ID: faultId};
+                const legendMatRequest = {Fault_ID: faultId, ID: faultId};
                 legendMatRequest['item'] = material.name;
                 legendMatRequest['itemType'] = material.category.id;
                 legendMatRequest['category'] = itemTypes[material.category.id].category_code;
@@ -126,6 +129,7 @@ module.exports = (function () {
                 legendMatRequest['supervisor'] = "1263";//default
                 _options.json = legendMatRequest;
                 request.post(`${BASE_URL}/requests`, _options, (err, res, body) => {
+                    console.log('BODY', body);
                     if (err || res.statusCode !== 200) return reject(err || res.statusCode);
                     return resolve(body);
                 });
